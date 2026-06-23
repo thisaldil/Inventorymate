a46fb3702dc975a9aea1e27fcdabf09aaadc462b
Thisal Gonsalkorala <tdimith10@gmail.com>
2026-06-23 09:57:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+dns.setDefaultResultOrder('ipv4first');
 import app from './app.js';
 import { connectDB } from './config/db.js';
 import { env } from './config/env.js';
